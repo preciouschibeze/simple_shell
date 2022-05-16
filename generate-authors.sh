3cf655650884b8c2de4d6f1e2b2d7b4015afe7b7
preciouschibeze <99445489+preciouschibeze@users.noreply.github.com>
2022-05-16 14:22:54 +0100

    Create generate-authors.sh

---
+++ generate-authors.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+# shellcheck disable=SC2128
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
+
+{
+	cat <<-'EOH'
+	# This file lists all individuals having contributed content to the repository.
+	# For how it is generated, see `generate-authors.sh`.
+	EOH
+	echo
+	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
+} > AUTHORS
